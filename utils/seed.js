aeed19be33ce2608080995b39c784318662625a1
millerdotkyle <kylenotfound@gmail.com>
2023-10-15 02:09:31 -0400

    fixing seeds

--- utils/seed.js
+++ utils/seed.js
@@ -1,28 +1,23 @@
 const connection = require('../config/connection');
-const { Post, User } = require('../models');
+const { Thought, User } = require('../models');
 const getRandomName = require('./data');
 
 console.log(getRandomName());
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
-  console.log('connected');
-  await Post.deleteMany({});
+  await Thought.deleteMany({});
   await User.deleteMany({});
+  const users = await User.insertMany([
+    {
+      username: getRandomName(),
+      email: '',
+      thoughts: [],
+      friends: [],
+    },
+  });
+    
 
-  const users = [];
-
-  for (let i = 0; i < 20; i++) {
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-
-    users.push({
-      first,
-      last,
-      age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
-    });
-  }
 
   await User.collection.insertMany(users);
   console.log(users);
